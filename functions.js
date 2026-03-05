31dc8f86d40e7a37a1e7a0356ea998a5e59a0f7c
Gabriel100-Pro <cortesempreender2.0@gmail.com>
2026-03-04 23:50:47 -0300

    Create banner

--- functions.js
+++ functions.js
@@ -1,15 +1,48 @@
-function sortear(){
+
+
+function numerosorteado(){
     const minimo = Math.ceil(document.getElementById("min").value);
     const maximo = Math.floor(document.getElementById("max").value);
-    
-
-     if (maximo <=  minimo) {   
+    const banner = document.getElementById("banner");
+    const spinner = document.getElementById("spinner");
+    const mensagem = document.getElementById("mensagem");
+    const valorSorteado = document.getElementById("valor-sorteado");
+if (maximo <=  minimo){
         alert("O valor máximo deve ser maior que o valor mínimo.");
         return false;
-    }else{
+}else{
         const resultado = Math.floor(Math.random() * (maximo - minimo + 1)) + minimo;
-        alert(`Número sorteado: ${resultado}`);
-    }
+        valorSorteado.textContent = resultado;
 }
+    banner.style.display = "flex";
+    mensagem.style.display = "block";
+    spinner.style.display = "block";
 
-sortear();
+    // Simula tempo de download
+    setTimeout(() => {
+      mensagem.textContent = "Download realizado com sucesso!";
+      spinner.style.display = "none";
+
+      // Esconde o banner depois de 3 segundos
+      setTimeout(() => {
+        banner.style.display = "none";
+      }, 3000);
+
+    }, 3000);
+  }
+
+  function fecharBanner() {
+    const banner = document.getElementById("banner");
+    const spinner = document.getElementById("spinner");
+    
+
+    spinner.style.display = "none";
+    banner.style.display = "none";
+}
+const buttonbanner = document.querySelector(".buttonbanner");
+buttonbanner.addEventListener("click", fecharBanner);
+
+ sorteio.addEventListener("click", numerosorteado); 
+
+numerosorteado();
+
